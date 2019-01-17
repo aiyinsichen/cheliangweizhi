6bcb2a7bf7f0640a45696e41e7a0e76313a13f02
aiyinsichen <aiyinsichen@qq.com>
2019-01-17 11:22:02 +0800

    增加添加车辆信息功能

--- main.c
+++ main.c
@@ -1,38 +1,59 @@
 #include <stdio.h>
 
-int main(){
+    struct point{
+        int x;
+        int y;
+        };
+        int main()
 
-    printf("huanyingjinrucheliangweizhiguanlixitong\n");
-    while(1){
-    printf("1--tianjiayixiecheliangweizhixinxi\n");
-    printf("2--shanchucheliangweizhixinxi\n");
-    printf("3--xianshisuoyoucheliangweizhixinxi\n");
-    printf("4--liwozuijindecheliangweizhixinxi\n");
-    printf("5--tuichuxitong\n");
+    {
+        int index = 0;
+        struct point arr[100];
 
-
-    printf("qingshuruxiangyingdegongnengbianhao\n");
-    int code;
-    scanf("%d",&code);
-
-    if(code == 1){
-
-    } 
-    if(code == 2){
+        printf("huanyingjinrucheliangweizhiguanlixitong\n");
         
-    }
-    if(code == 3){
-        
-    }
-    if(code == 4){
-        
-    }
-    if(code == 5){
-        
-    }
+        while (1)
+        {
+            printf("1--tianjiayixiecheliangweizhixinxi\n");
+            printf("2--shanchucheliangweizhixinxi\n");
+            printf("3--xianshisuoyoucheliangweizhixinxi\n");
+            printf("4--liwozuijindecheliangweizhixinxi\n");
+            printf("5--tuichuxitong\n");
 
+            printf("qingshuruxiangyingdegongnengbianhao\n");
+            int code;
+            scanf("%d", &code);
 
+            if (code == 1)
+            {
+                printf("qingshurucheliangweizhixheyzuobiao：\n");
+                int x;
+                int y;
+                scanf("%d", &x);
+                scanf("%d", &y);
+
+                arr[index].x = x;
+                arr[index].y = y;
+
+                index++;
+
+                printf("tianjiawancheng,dianjihuichejixu\n");
+                char xx;
+                scanf("%c", &xx);
+                scanf("%c", &xx);
+            }
+            if (code == 2)
+            {
+            }
+            if (code == 3)
+            {
+            }
+            if (code == 4)
+            {
+            }
+            if (code == 5)
+            {
+            }
+        }
+        return 0;
     }
-    return 0;
-
-}
