bee94535d7e0918230476c09eb9549126457ea4f
aiyinsichen <aiyinsichen@qq.com>
2019-01-17 11:02:29 +0800

    构建车辆管理系统的基本构架

--- main.c
+++ main.c
@@ -2,8 +2,37 @@
 
 int main(){
 
-    printf("hello world!");
+    printf("huanyingjinrucheliangweizhiguanlixitong\n");
+    while(1){
+    printf("1--tianjiayixiecheliangweizhixinxi\n");
+    printf("2--shanchucheliangweizhixinxi\n");
+    printf("3--xianshisuoyoucheliangweizhixinxi\n");
+    printf("4--liwozuijindecheliangweizhixinxi\n");
+    printf("5--tuichuxitong\n");
 
+
+    printf("qingshuruxiangyingdegongnengbianhao\n");
+    int code;
+    scanf("%d",&code);
+
+    if(code == 1){
+
+    } 
+    if(code == 2){
+        
+    }
+    if(code == 3){
+        
+    }
+    if(code == 4){
+        
+    }
+    if(code == 5){
+        
+    }
+
+
+    }
     return 0;
 
 }
